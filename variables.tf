05dbc992b8418b9cc1e22917026f1aa60bb3b279
Johnathan Bangura <jbangura@vt.edu>
2023-08-29 02:07:31 -0400

    first commit

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "vpc_id" {
+  type = string
+
+}
+
+variable "my_ip_with_cidr" {
+  type        = string
+  description = "Provide your IP eg, 104.194.51.113/32"
+
+}
+
+variable "public_key" {
+  type        = string
+  description = "Provide your public key"
+
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+
+}
+
+variable "server_name" {
+  type        = string
+  description = "Provide a name for your server"
+}
